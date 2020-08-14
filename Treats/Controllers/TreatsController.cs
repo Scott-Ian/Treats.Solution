a3b656ea2d395e5dbcad51c7967000af663ed659
Ian Scott <chesnekov@gmail.com>
2020-08-14 10:32:23 -0700

    adds Index route to TreatsController.cs. Allows sorting of list depending on input

--- Treats/Controllers/TreatsController.cs
+++ Treats/Controllers/TreatsController.cs
@@ -22,6 +22,51 @@ namespace Treats.Controllers
       _userManager = userManager;
       _db = db;
     }
-    
+
+    public ActionResult Index(string sortOrder, string searchString)
+    {
+      ViewBag.NameSortParam = string.IsNullOrEmpty(sortOrder) ?"last_desc" : "";
+      ViewBag.IdSortParam = sortOrder == "Id" ? "id_desc" : "Id";
+      ViewBag.DescriptionSortParam = sortOrder == "Descr" ? "descr_desc" : "Descr";
+      ViewBag.PriceSortParam = sortOrder == "Price" ? "price_desc" : "Price";
+
+      IQueryable<Treat> treats = _db.Treats
+        .Include(treat => treat.Flavors)
+        .ThenInclude(join => join.Flavor);
+
+      if (!string.IsNullOrEmpty(searchString))
+      {
+        treats = treats.Where(treat => treat.Name.Contains(searchString));
+      }
+
+      switch (sortOrder)
+      {
+        case "last_desc":
+          treats = treats.OrderByDescending(treat => treat.Name);
+          break;
+        case "Id":
+          treats = treats.OrderBy(treat => treat.TreatId);
+          break;
+        case "id_desc":
+          treats = treats.OrderByDescending(treat => treat.TreatId);
+          break;
+        case "Descr":
+          treats = treats.OrderBy(treat => treat.Description);
+          break;
+        case "descr_desc":
+          treats = treats.OrderByDescending(treat => treat.Description);
+          break;
+        case "Price":
+          treats = treats.OrderBy(treat => treat.Price);
+          break;
+        case "price_desc":
+          treats = treats.OrderByDescending(treat => treat.Price);
+          break;
+        default:
+          treats = treats.OrderBy(treat => treat.Name);
+          break;
+      }
+      return View(treats.ToList());
+    }
   }
 }
