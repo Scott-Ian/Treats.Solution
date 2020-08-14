07449f56e1207bb1df07990e12e1fbd2ee72fda1
Ian Scott <chesnekov@gmail.com>
2020-08-14 10:45:03 -0700

    adds Edit get and post routes to TreatsController.cs

--- Treats/Controllers/TreatsController.cs
+++ Treats/Controllers/TreatsController.cs
@@ -91,5 +91,19 @@ namespace Treats.Controllers
       return ViewModels(thisTreat);
     }
 
+    public ActionResult Edit (int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Entry(treat).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
